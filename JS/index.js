7ef1c88f034856c6cd69f1cbd6798f93da350bdb
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-01 09:55:22 -0300

    commit

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,6 @@
-console.log("teste");
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+// Funções
+
+// Eventos
