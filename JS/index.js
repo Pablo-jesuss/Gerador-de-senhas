2fa895604f8059faa345af2cb6127fd32dd9234f
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-04 10:18:07 -0300

    getLetter

--- JS/index.js
+++ JS/index.js
@@ -2,5 +2,14 @@
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
 // Funções
+const getLetterLowerCase = () => {
+  return String.fromCodePoint(Math.floor(Math.random() * 26) + 97);
+};
 
+const getLetterUpperCase = () => {
+  return String.fromCodePoint(Math.floor(Math.random() * 26) + 65);
+};
 // Eventos
+generatePasswordButton.addEventListener("click", () => {
+  console.log("teste");
+});
