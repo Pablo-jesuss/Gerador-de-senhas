b71162a78686be55a4b81b400109a374d8212558
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-04 10:45:54 -0300

    password

--- JS/index.js
+++ JS/index.js
@@ -19,8 +19,38 @@ const getSymbol = () => {
   return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
-console.log(getSymbol());
+const generatePassword = (
+  getLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbol
+) => {
+  let password = "";
+
+  const passwordLength = 10;
+
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  ];
+
+  for (i = 0; i < passwordLength; i = i + 4) {
+    generators.forEach(() => {
+      const randomValue =
+        generators[Math.floor(Math.random() * generators.length)]();
+
+      password += randomValue;
+    });
+  }
+};
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
-  console.log("teste");
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  );
 });
