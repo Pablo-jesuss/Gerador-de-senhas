1dba2bbdc2fa57d065f0f487d95518722c942e00
Pablo-jesuss <pablo.programador26@gmail.com>
2024-03-04 10:31:46 -0300

    getSymbol

--- JS/index.js
+++ JS/index.js
@@ -9,6 +9,17 @@ const getLetterLowerCase = () => {
 const getLetterUpperCase = () => {
   return String.fromCodePoint(Math.floor(Math.random() * 26) + 65);
 };
+
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+  const symbols = "(){}[]=<>/,.!@#$%&*+-";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+console.log(getSymbol());
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
   console.log("teste");
